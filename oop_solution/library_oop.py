fda306d61040892c48517135b3dd9bb7045590a3
theewasu <theewasu_a@ku.th>
2025-11-13 13:18:50 +0700

    commit 2

---
+++ oop_solution/library_oop.py
@@ -0,0 +1,6 @@
+class Book:
+    def __init__(self, id, title, author, available_copies):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.available_copies = available_copies
