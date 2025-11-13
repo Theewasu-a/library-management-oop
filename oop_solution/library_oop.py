a5ccf22ff32e25191016dbbbb5117f67a51dfc00
theewasu <theewasu_a@ku.th>
2025-11-13 13:22:59 +0700

    commit 3

--- oop_solution/library_oop.py
+++ oop_solution/library_oop.py
@@ -4,3 +4,10 @@ class Book:
         self.title = title
         self.author = author
         self.available_copies = available_copies
+
+class Member:
+    def __init__(self, id, name, email="N/A"):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.borrowed_books = []
